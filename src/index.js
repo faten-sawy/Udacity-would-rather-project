16bb8a494e49594cff39918ecc939486a11415eb
faten-sawy <75747748+faten-sawy@users.noreply.github.com>
2021-05-02 08:00:07 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ import middleware from './middleware'
 const store = createStore(reducer,middleware)
 
 ReactDOM.render( 
-  <Provider store = {store}>
-    <App />
-  </Provider>,
+  
+    <Provider store = {store}>
+      <App />       
+    </Provider>  
+ ,
   document.getElementById('root')
 );
